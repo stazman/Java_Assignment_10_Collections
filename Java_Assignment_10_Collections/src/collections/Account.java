a4ff478ef57e1ed53e2b9df7ab14310354728232
Christopher DiStasio <cadistasio@gmail.com>
2020-05-14 14:31:35 -0500

    Creates Account class, constructor; stubs createAccounts method

---
+++ Java_Assignment_10_Collections/src/collections/Account.java
@@ -0,0 +1,31 @@
+package collections;
+
+import java.util.List;
+
+public class Account {
+
+	long id;
+	
+	String accountType;
+	
+	public Account(long id, String accountType) {
+	
+		super();
+		
+		this.id = id;
+		this.accountType = accountType;
+		
+	};
+	
+	List<Account> createAccounts(int numAccounts){
+		
+		// create a new Account each time 
+		// 
+		
+		
+		
+		return null;
+		
+	};
+	
+}
