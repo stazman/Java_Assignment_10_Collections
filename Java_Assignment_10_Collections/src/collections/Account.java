f745b73d7194e37dc62b6e251ab1ae0ad3afa029
Christopher DiStasio <cadistasio@gmail.com>
2020-07-21 23:26:12 -0500

    Stubs Account, application files

--- Java_Assignment_10_Collections/src/collections/Account.java
+++ Java_Assignment_10_Collections/src/collections/Account.java
@@ -19,6 +19,10 @@ public class Account {
 	
 	List<Account> createAccounts(int numAccounts){
 		
+//		= new Account();
+//		The method should create a number of Accounts equal to the numAccounts parameter and return those accounts in a List. For example, createAccounts(5) should create five instances of Account and return those in a List.
+//				In an Application class, call the createAccounts() and then loop through the returned List and print the ids of each Account.
+		
 		// create a new Account each time 
 		// 
 		
---
+++ Java_Assignment_10_Collections/src/collections/Application.java
@@ -0,0 +1,13 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> arr=new ArrayList<Integer>(10);
+
+	}
+
+}
